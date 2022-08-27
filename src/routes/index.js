408f5320816259c8c5952e84382155b4fa7e24bf
JorgeDanielVital <u201718971@upc.edu.pe>
2022-08-27 18:16:18 -0500

    sede fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,16 @@
 const {response, request} = require("express");
 const {Router} = require('express');
-const { getAforo, getSedes, getTSedesController, cambiarAforo } = require("../controllers/Sede");
+const { getAforo, getSedes, getTSedesController, updateAforo } = require("../controllers/Sede");
 const { model_getDepartamentoById, model_getTDepartamentos } = require("../controllers/Departamento"); 
 const { model_getProvinciaByDepa, model_getTProvincias }  = require("../controllers/Provincia");
 const { model_getDistritoByProv , model_getTDistritos } = require("../controllers/Distrito");  
 
 const router = Router();
+
+router.get('/Sede/getAforo', getAforo);
+router.get('/Sede/getSedes', getSedes);
+router.get('/Sede/getTSedes', getTSedesController);
+router.post('/Sedes/updateAforo', updateAforo);
 
 router.get('/Departamento/getTDepartamentos', model_getTDepartamentos);
 
@@ -16,6 +21,8 @@ router.get('Provicina/getTProvincias', model_getTProvincias);
 // Distrito 
 router.get('Distrito/getDistritoByProv/:prov_id', model_getDistritoByProv); 
 router.get('Distrito/getTDistritos', model_getTDistritos); 
+
+
 
 
 module.exports= router;

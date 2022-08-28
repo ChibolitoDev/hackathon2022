4b76c5c72f3c46a39ad7ab41400d245b8d40c625
Jean Carlo <jeanca2198@gmail.com>
2022-08-28 12:15:34 -0500

    add cors config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT
 
+app.use(cors({
+    origin: ["*"]
+}))
 app.use(express.json());
 app.use(require('./routes/index'))
 
